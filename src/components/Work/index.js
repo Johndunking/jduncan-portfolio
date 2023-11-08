d07010ee9d00b1e909d38acd84c26ff3a9929fe5
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2023-11-07 21:29:24 -0800

    work page edit

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -79,12 +79,12 @@ const Work = () => {
       src="https://images.unsplash.com/reserve/oIpwxeeSPy1cnwYpqJ1w_Dufer%20Collateral%20test.jpg?auto=format&fit=crop&q=80&w=2815&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
        />
       <Card.Body>
-        <Card.Title>S.T.O.R.T.E</Card.Title>
+        <Card.Title>HandyDude</Card.Title>
         <Card.Text>
-          Handy man website built for local handyman that supports our school and community.
+         Handyman website template built for small businesses. <br /> I can personalize logo, images and add additional features! < br/> Please reach out if interested!
         </Card.Text>
-        <Button backgroundColor="#F08080"  variant="primary" href="https://storte.netlify.app"
-        target="_blank">S.T.O.R.T.E</Button>
+        <Button backgroundColor="#F08080"  variant="primary" href="https://handydude.netlify.app"
+        target="_blank">HandyDude</Button>
       </Card.Body>
       
     </Card>
